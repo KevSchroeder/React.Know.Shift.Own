de9b0f8c46161663ea4fad77cabd63b8834db5a3
Kevin Schroeder <95885765+KevSchroeder@users.noreply.github.com>
2023-12-09 15:14:15 -0500

    diagonal text

--- pages/index.js
+++ pages/index.js
@@ -23,37 +23,24 @@ export default function Home() {
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover"
         />
-        <div
-          className="absolute -top-80 left-[max(6rem,33%)] -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56"
-          aria-hidden="true"
-        >
-          <div
-            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-            style={{
-              clipPath:
-                'polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)',
-            }}
-          />
-        </div>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:mt-10 lg:max-w-none lg:grid-cols-12">
-            <div className="relative lg:order-last lg:col-span-5">            
-            </div>
-            <div className="max-w-xl text-base leading-7 text-gray-700 lg:col-span-7">
+        <div className="grid grid-cols-3 grid-rows-3">
+            <div className="max-w-xl text-base leading-7 text-gray-700 lg:col-span-5 px-12">
               <h2 className="mt-14 text-2xl font-bold tracking-tight text-gray-900">
                 KNOW Your Body
               </h2>
               <p className="mt-8">
                 Discover the science and innate treasures of your
                 well-being and be the power you and your vision need to succeed.
-              </p>
+              </p></div>
+              <div className="col-start-2 px-12">
               <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
                 SHIFT Your Mind
               </h2>
               <p className="mt-8">
                 Transform your standard operating system into the
                 unique set of habits and strategies for your exponential growth.
-              </p>
+              </p></div>
+              <div className="row-start-3 col-start-3 px-12">
               <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
                 OWN Your Future
               </h2>
@@ -61,7 +48,6 @@ export default function Home() {
                 Empowering you, your business, and your vision for
                 sustained growth, profitability, and impact.
               </p>
-            </div>
           </div>                       
         </div>
         <div className="mx-auto max-w-4xl px-6 lg:px-8">
